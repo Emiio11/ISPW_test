7219794a3c68684352fcfb635df26a50ba4b6418
alessiocarta <cartaalessio02@gmail.com>
2025-10-06 14:14:18 +0200

    cambio classe

--- src/codice/Main.java
+++ src/codice/Main.java
@@ -1,8 +1,8 @@
-package codice;
+
 
 public class Main {
     public static void main(String[] args) {
-        int c= Prodotto.prodotto(1,2);
+        int c= codice.Prodotto.prodotto(1,2);
         System.out.println(c);
     }
 }
