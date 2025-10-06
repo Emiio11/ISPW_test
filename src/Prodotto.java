2923f19f959a6e3b73a06c5a1a7c22746d5517fc
alessiocarta <cartaalessio02@gmail.com>
2025-10-06 13:02:18 +0200

    cambio classe

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,10 +1,7 @@
 
 
 public class Prodotto{
-    private int x;
-
-    int prodotto(int a, int b){
-        this.x=a*b;
-        return this.x;
+    public static int prodotto(int a, int b){
+        return a*b;
     }
 }
