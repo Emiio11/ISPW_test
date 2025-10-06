7fe94ac19906e5f6c369c2d0bf9a946af993aa8b
alessiocarta <cartaalessio02@gmail.com>
2025-10-06 13:03:32 +0200

    cambio classe

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto a = new Prodotto();
-        int c=a.prodotto(1,2);
+        int c= Prodotto.prodotto(1,2);
         System.out.println(c);
-        int x=5;
-        int y= 7;
-        int somma = Util.somma(x,y);
-        System.out.println(somma);
 
     }
 }
