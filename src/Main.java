9ac0339de9faebc88c39cfe66250cfeb7a6fe4a0
Emilio Tulli <emiliotulli03@gmail.com>
2025-10-06 12:57:23 +0200

    somma

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,10 @@ public class Main {
         Prodotto a = new Prodotto();
         int c=a.prodotto(1,2);
         System.out.println(c);
+        int x=5;
+        int y= 7;
+        int somma = Util.somma(x,y);
+        system.out.printl(somma);
+
     }
 }
---
+++ src/somma.java
@@ -0,0 +1,5 @@
+public class Util{
+    public static int somma(int a,int b){
+        return a+b;
+    }
+}
