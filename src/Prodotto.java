6ea62be0af3309e9790a93453a6e9acd474d9389
alessiocarta <cartaalessio02@gmail.com>
2025-10-06 12:46:08 +0200

    cambio classe

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,4 +1,6 @@
-public class prodotto{
+package src.Prodotto;
+
+public class Prodotto{
     prodotto(int x,int y){
         this.x=x*this.y;
     }
---
+++ src/main.java
@@ -0,0 +1,10 @@
+package src.main;
+import src.*;
+
+class main {
+    public static void main(String[] args) {
+        Prodotto a = new Prodotto();
+        int c=a.prodotto(1,2);
+        System.out.println(c);
+    }
+}
